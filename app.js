1c92df395655090c4409a24814b3c50d10558f13
Peter Morjan <pmorjan@gmail.com>
2016-04-09 13:16:55 +0200

    make writing logfile optional

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const ssh = require('./lib/ssh')
 
 const defaults = {
   title: 'ssh-live',
-  port: 8080,
-  logfile: path.join(os.tmpdir(), 'ssh-live.log')
+  port: 8080
 }
 
 //
@@ -25,7 +24,7 @@ const defaults = {
 //
 cmd
   .usage('[options] [hop...] user@hostname[:port]')
-  .option('-l, --logfile <filename>', 'logfile - defaults to ' + defaults.logfile, defaults.logfile)
+  .option('-l, --logfile <filename>', 'log http request to file')
   .option('-p, --port <port>', 'http listen port - defaults to ' + defaults.port, defaults.port, parseInt)
   .on('--help', function () {
     console.log('  Example:')
@@ -50,11 +49,13 @@ if (cmd.port < 1024 && process.geteuid() !== 0) {
 // app
 //
 process.title = defaults.title
-const logStream = fs.createWriteStream(cmd.logfile, {flags: 'a'})
 const app = express()
 app.use(helmet())
 app.use(compression())
-app.use(morgan('common', {stream: logStream}))
+if (cmd.logfile) {
+  const logStream = fs.createWriteStream(cmd.logfile, {flags: 'a'})
+  app.use(morgan('common', {stream: logStream}))
+}
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'public/index.html'))
@@ -66,7 +67,8 @@ app.get('/', function (req, res) {
 const server = http.createServer(app)
 
 server.on('error', function (err) {
-  console.log('server error:', err)
+  console.log('error:', err.message)
+  process.exit(1)
 })
 
 server.listen(cmd.port, function () {
@@ -89,19 +91,17 @@ wss.broadcast = function broadcast (data) {
 wss.on('connection', function (ws) {
   ws.send('\r\n\x1b[33m Welcome to the show!\x1b[0m\r\n\r\n')
 
-  process.stdout.write('\x1b]0; new connection\x07')
-
   ws.on('close', function () {
     ws.terminate()
   })
 
   ws.on('error', function (err) {
-    log('error:', err.stack)
+    console.log('error:', err.stack)
   })
 })
 
 wss.on('error', function (err) {
-  log('wss error:', err.stack)
+  console.log('wss error:', err.stack)
 })
 
 //
@@ -114,7 +114,7 @@ function startSSH () {
       process.exit(0)
     })
     .on('error', function (err) {
-      console.error(err)
+      console.error('error:', err.message)
       process.exit(1)
     })
     .on('info', function (info) {
@@ -135,10 +135,5 @@ function printUrlList () {
    .map((ip) => util.format(' http://%s:%s', ip, cmd.port))
    .join('\n')
   console.log('server listening on:\n%s\n', urls)
-}
-
-function log () {
-  console.log('%s - %s', new Date().toISOString(),
-    util.format.apply(util.format, arguments))
 }
 
